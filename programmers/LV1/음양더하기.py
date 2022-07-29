c543ba672ba6d12da615c038055eabcd0e6431cb
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-07-29 14:36:59 +0900

    해결 다시보기(리스트 컴프리핸션 if,else)

--- programmers/LV1/음양더하기.py
+++ programmers/LV1/음양더하기.py
@@ -1,10 +1,14 @@
 absolutes = 	[1, 2, 3]
-signs = ['false','false','true']
-def solution(absolutes, signs):
+signs = [False, False, True]
+# def solution(absolutes, signs):
+#
+#     for i in range(len(absolutes)):
+#         if signs[i] == False:
+#             absolutes[i] = absolutes[i] * -1
+#     answer = sum(absolutes)
+#     return answer
 
-    for i in range(len(absolutes)):
-        if signs[i] == False:
-            absolutes[i] = absolutes[i] * -1
-    answer = sum(absolutes)
-    return answer
 
+def solution(a,s):
+    return sum([i if j else -i  for i, j in zip(a,s) ])
+print(solution(absolutes,signs ))
