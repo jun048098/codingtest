4ccea88bff05e89dfd02c64f6c345df9a1c806ec
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-07-29 14:44:26 +0900

    해결 다시보기(내적 넘파이, zip)

--- programmers/LV1/내적.py
+++ programmers/LV1/내적.py
@@ -1,5 +1,9 @@
 a =[-1,0,1]
 b = [1,0,-1]
-answer = sum([a * b for a,b in zip(a,b)])
-print(answer)
+# import numpy as np
+# def solution(a,b):
+#     return sum((np.array(a) * np.array(b)).tolist())
 
+def solution(a,b):
+    return sum([i*j for i,j in zip(a,b)])
+print(solution(a,b))
