2427610cc36b3314865bd82d1bd2aa1b1a1a97bd
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-07-29 14:33:51 +0900

    해결

--- programmers/LV1/없는 숫자 더하기.py
+++ programmers/LV1/없는 숫자 더하기.py
@@ -1,8 +1,11 @@
 numbers = [1,2,3,4,6,7,8,0]
-answer = 0
-for i in range(10):
-        if i in numbers:
-            print(i)
-        else:
-            answer += i
-print(answer)
+# answer = 0
+# for i in range(10):
+#         if i in numbers:
+#             print(i)
+#         else:
+#             answer += i
+# print(answer)
+
+def solution(n):
+    return sum([i for i in range(10) if i not in n])
