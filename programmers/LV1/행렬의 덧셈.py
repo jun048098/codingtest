14dcf5a50afc74e2c05dc3e9b4e44b79a7bfacc9
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-07-29 13:08:49 +0900

    해결 다시보기

--- programmers/LV1/행렬의 덧셈.py
+++ programmers/LV1/행렬의 덧셈.py
@@ -1,11 +1,17 @@
-def solution(A, B):
-    # answer = [[c +d for c, d in zip(a,b)] for a,b in zip(A,B)]
-    answer = [[list(map(sum, zip(*x)))] for x in zip(A,B)]
-    return answer
-print(solution([[1,2], [2,3]], [[3,4],[5,6]]))
+# def solution(A, B):
+#     # answer = [[c +d for c, d in zip(a,b)] for a,b in zip(A,B)]
+#     answer = [[list(map(sum, zip(*x)))] for x in zip(A,B)]
+#     return answer
+# print(solution([[1,2], [2,3]], [[3,4],[5,6]]))
 
 
 
 # print(list(map(sum, zip(*([1, 2], [3, 4])))))
 
     # , ([2, 3], [5, 6])]
+
+
+def solution(a,b):
+    return [[e+f for e,f in zip(c,d)]for c,d in zip(a,b)]
+
+print(solution([[1,2], [2,3]], [[3,4],[5,6]]))
