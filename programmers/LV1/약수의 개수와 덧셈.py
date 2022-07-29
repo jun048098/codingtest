087e7129ab6f3e8a76c1d6274a64e12d3c56f793
jun048098 <96534680+jun048098@users.noreply.github.com>
2022-07-29 17:09:34 +0900

    미해결 다시보기(약수문제)

--- programmers/LV1/약수의 개수와 덧셈.py
+++ programmers/LV1/약수의 개수와 덧셈.py
@@ -1,19 +1,27 @@
+# def solution(left, right):
+#     num_list = [i for i in range(left, right + 1)]
+#     d = []  # 약수개수
+#
+#     for i in num_list:
+#         cnt = []
+#         for j in range(1, i + 1):
+#             if i % j == 0:
+#                 cnt.append(j)
+#         d.append(len(cnt))
+#     #
+#     for i in range(len(d)):
+#         if d[i] % 2 == 1:
+#             num_list[i] = num_list[i] * (-1)
+#     answer = sum(num_list)
+#
+#     return answer
+
 def solution(left, right):
-    num_list = [i for i in range(left, right + 1)]
-    d = []  # 약수개수
-
-    for i in num_list:
-        cnt = []
-        for j in range(1, i + 1):
-            if i % j == 0:
-                cnt.append(j)
-        d.append(len(cnt))
-    #
-    for i in range(len(d)):
-        if d[i] % 2 == 1:
-            num_list[i] = num_list[i] * (-1)
-    answer = sum(num_list)
-
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
     return answer
-
 print(solution(24,	27))
