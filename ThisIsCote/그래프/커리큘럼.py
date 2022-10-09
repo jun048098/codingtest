b3a83aa46fa25726aec27fc6f1f496e35dd2f6c1
jun048098 <jun048098@gmail.com>
2022-10-09 23:01:45 +0900

    위상 정렬

---
+++ ThisIsCote/그래프/커리큘럼.py
@@ -0,0 +1,50 @@
+# # 위상정렬
+from collections import deque
+import copy
+
+# 노드의 개수 입력
+v = int(input())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0] * (v+1)
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트(그래프) 초기화
+graph = [[] for _ in range(v+1)]
+# 강의 시간
+time = [0] * (v+1)
+# 방향그래프 간선 입력
+for i in range(1,v+1):
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        indegree[i] += 1
+        graph[x].append(i)
+
+# 위상 정렬 함수
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
+
+    # 처음 시작할 때는 진입 차수가 0인 노드를 큐에 삽입
+    for i in range(1, v+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i], result[now]+ time[i])
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+    for i in range(1, v+1):
+        print(result[i])
+
+topology_sort()
+
+'''
+5
+10 -1
+10 1 -1
+4 1 -1
+4 3 1 -1
+3 3 -1
+'''
