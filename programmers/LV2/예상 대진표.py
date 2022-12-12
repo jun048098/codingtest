47351d8288930f8599a201ff8be9756c77542620
jun048098 <jun048098@gmail.com>
2022-12-12 19:15:57 +0900

    .

--- programmers/LV2/예상 대진표.py
+++ programmers/LV2/예상 대진표.py
@@ -1,7 +1,13 @@
-def solution(n,a,b):
-    answer = 0
-    while a != b:
-        a = (a+1) // 2
-        b = (b+1) // 2
-        answer +=1
-    return answer
+# def solution(n):
+#     ans = 0
+#     while n!=0:
+#         if n % 2 == 0:
+#             n //= 2
+#         else:
+#             n -= 1
+#             ans +=1
+
+#     return ans
+
+def solution(n):
+    return bin(n).count('1')
