575d1c146bb945d3b958ca2c579d0ff68bb44d4d
jun048098 <jun048098@gmail.com>
2023-09-19 15:28:15 +0900

    heapq 솔

---
+++ baekjun/etc/최소 힙_1927.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+q = []
+
+for _ in range(n):
+    k = int(input())
+    if k == 0:
+        if q:
+            print(heapq.heappop(q))
+        else:
+            print(0)
+    else:
+        heapq.heappush(q, k)
