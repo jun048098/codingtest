d837b304a71afea02ce4f69c9018eb338a631e47
jun048098 <jun048098@gmail.com>
2023-09-25 23:14:57 +0900

    플로이드 워셜 수정

--- baekjun/graph/graph_11403_경로찾기.py
+++ baekjun/graph/graph_11403_경로찾기.py
@@ -46,8 +46,8 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            if graph[i][j] and graph[k][i]:
-                graph[k][j] = 1
+            if graph[j][i] and graph[i][k]:
+                graph[j][k] = 1
 
 for i in graph:
     print(*i)
