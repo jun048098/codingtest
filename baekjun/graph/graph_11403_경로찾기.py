a34a3046add3ecef045410adbfa4b1374a3efdf4
jun048098 <jun048098@gmail.com>
2023-09-25 16:47:15 +0900

    dfs,bfs,플로이드 워셜 솔

---
+++ baekjun/graph/graph_11403_경로찾기.py
@@ -0,0 +1,53 @@
+# bfs
+# from collections import deque
+# n = int(input())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# visit = [[0] * n for _ in range(n)]
+#
+# def bfs(start):
+#     q = deque()
+#     q.append(start)
+#     check = [0 for _ in range(n)]
+#
+#     while q:
+#         now = q.popleft()
+#         for i in range(n):
+#             if check[i] == 0 and graph[now][i] == 1:
+#                 q.append(i)
+#                 check[i] = 1
+#                 visit[start][i] = 1
+#
+# for i in range(n):
+#     bfs(i)
+# for i in visit:
+#     print(*i)
+
+# dfs
+# n = int(input())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# visit = [0 for _ in range(n)]
+#
+# def dfs(start):
+#     for i in range(n):
+#         if graph[start][i] == 1 and visit[i] == 0:
+#             visit[i] = 1
+#             dfs(i)
+#
+# for i in range(n):
+#     dfs(i)
+#     print(*visit)
+#     visit = [0 for i in range(n)]
+
+
+# 플로이드 워셜
+n =  int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if graph[i][j] and graph[k][i]:
+                graph[k][j] = 1
+
+for i in graph:
+    print(*i)
